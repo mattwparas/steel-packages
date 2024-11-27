47804bebc7977eda4f5d2ad3684e099af3aef369
Matt Paras <matthewparas2020@u.northwestern.edu>
2024-11-27 11:50:55 -0800

    first commit

---
+++ packages.scm
@@ -0,0 +1,5 @@
+(define package-index
+  (hash 'steel-postgres
+        "https://github.com/mattwparas/steel-postgres"
+        'steel-datafusion
+        "https://github.com/mattwparas/steel-datafusion"))
