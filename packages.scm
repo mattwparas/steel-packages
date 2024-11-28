98a4812cb2122193d3ba2f8010a3274bca9227b1
Matt Paras <matthewparas2020@u.northwestern.edu>
2024-11-27 21:24:38 -0800

    fix package name

--- packages.scm
+++ packages.scm
@@ -6,4 +6,4 @@
         'steel-datafusion
         (hash #:url "https://github.com/mattwparas/steel-datafusion")
         'steel-sqlite
-        (hash #:url "https://github.com/mattwparas/steel-sqlite" #:path "libs/steel-sqlite")))
+        (hash #:url "https://github.com/mattwparas/steel" #:path "libs/steel-sqlite")))
