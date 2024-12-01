4a4d4757b6959e8bd357159ba1beaa187e41a146
Matt Paras <matthewparas2020@u.northwestern.edu>
2024-12-01 09:39:04 -0800

    add more packages

--- packages.scm
+++ packages.scm
@@ -6,4 +6,30 @@
         'steel-datafusion
         (hash #:url "https://github.com/mattwparas/steel-datafusion")
         'steel-sqlite
-        (hash #:url "https://github.com/mattwparas/steel" #:path "libs/steel-sqlite")))
+        (hash #:url "https://github.com/mattwparas/steel" #:path "libs/steel-sqlite")
+        'srfi
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/srfi")
+        'steel/fs
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/fs")
+        'steel/collections
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/collections")
+        'steel/clos
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/clos")
+        'steel/lists
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/lists")
+        'steel/command-line
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/command-line")
+        'steel/logging
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/logging")
+        'steel/colors
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/colors")
+        'steel/time
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/time")
+        'steel/fs
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/fs")
+        'steel/sorting
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/sorting")
+        'steel/tests
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/tests")
+        'steel/coop
+        (hash #:url "https://github.com/mattwparas/steel" #:path "cogs/coop")))
